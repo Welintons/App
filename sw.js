8580fce5fa34782cc8293bc81f3882b6bb5fb573
WMjogos <149338055+Welintons@users.noreply.github.com>
2026-02-19 20:54:18 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,38 +1,10 @@
-// sw.js
-const CACHE_NAME = 'ws-gestao-v1';
-const ASSETS = [
-    '/',
-    '/index.html',
-    '/manifest.json',
-    '/sw.js',
-    'https://raw.githubusercontent.com/Welintons/Wsinfor/main/Gemini_Generated_Image_bqf58fbqf58fbqf5.png'
-];
+const cacheName = 'ws-gestao-v1';
+const assets = ['./', './index.html'];
 
-// Instalação do SW e cache
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-        .then(cache => cache.addAll(ASSETS))
-        .then(() => self.skipWaiting())
-    );
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(cacheName).then(cache => cache.addAll(assets)));
 });
 
-// Ativação do SW e limpeza de caches antigos
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => 
-            Promise.all(keys.map(key => {
-                if (key !== CACHE_NAME) return caches.delete(key);
-            }))
-        )
-    );
-    self.clients.claim();
-});
-
-// Intercepta requisições e retorna do cache se disponível
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-        .then(res => res || fetch(event.request))
-    );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
