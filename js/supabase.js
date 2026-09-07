051879bb34352d66e8d2c0925f6fce36fd89beb4
Ws soluções <149338055+Welintons@users.noreply.github.com>
2026-09-07 00:30:36 -0300

    Criar supabase.js

---
+++ js/supabase.js
@@ -0,0 +1,9 @@
+const SUPABASE_URL = "https://cojaeiatpulqqeelsofl.supabase.co";
+
+const SUPABASE_ANON_KEY =
+"sb_publishable_9WJMKxEBxETzP27dFSeBZA_Z4xz3Kqk";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_ANON_KEY
+);
